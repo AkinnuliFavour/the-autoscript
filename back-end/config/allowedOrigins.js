a9c01cb49b770adee95333ff660e719d1c6496c1
Gandort <oluwasemiloreakinnuli@gmail.com>
2023-12-26 12:25:26 +0100

    Update allowedOrigins.js with new frontend URL

--- back-end/config/allowedOrigins.js
+++ back-end/config/allowedOrigins.js
@@ -1,7 +1,6 @@
 const allowedOrigins = [
     'http://localhost:5173',
-    'https://www.dandrepairshop.com',
-    'https://dandrepairshop.com'
+    'https://the-autoscript-frontend.vercel.app/'
 ]
 
 module.exports = allowedOrigins;

cb3961cbd67942deacc554d8f01036cd8786cf7a
Gandort <oluwasemiloreakinnuli@gmail.com>
2023-12-29 02:30:28 +0100

    Update allowedOrigins.js to fix frontend URL

--- back-end/config/allowedOrigins.js
+++ back-end/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'http://localhost:5173',
-    'https://the-autoscript-frontend.vercel.app/file-transcrib/'
+    'https://the-autoscript-frontend.vercel.app/'
 ]
 
 module.exports = allowedOrigins;

3637d443ff73ee6b05a355823fc567804b3f1421
Gandort <oluwasemiloreakinnuli@gmail.com>
2023-12-29 10:58:57 +0100

    Add CORS headers to allow requests from specific domain

--- back-end/server.js
+++ back-end/server.js
@@ -32,6 +32,12 @@ const upload = multer({ storage: storage })
 
 app.use(cors(corsOptions))
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://the-autoscript-frontend.vercel.app/file-transcribe"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.json())
 
 app.use('/transcribe', upload.single('foo'), require('./routes/localFileTranscribe'))

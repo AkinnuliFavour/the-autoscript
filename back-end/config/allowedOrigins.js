5df8f6b44602d477d3a4877638b2ea813524ce50
Gandort <oluwasemiloreakinnuli@gmail.com>
2023-12-29 02:26:10 +0100

    Fix allowed origins and API endpoint URLs

--- back-end/config/allowedOrigins.js
+++ back-end/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'http://localhost:5173',
-    'https://the-autoscript-frontend.vercel.app/file-transcribe'
+    'https://the-autoscript-frontend.vercel.app/file-transcrib/'
 ]
 
 module.exports = allowedOrigins;

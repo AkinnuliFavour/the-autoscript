02ff4e5e1d42c23e32dfe2beb824a98ce29c45e0
Gandort <oluwasemiloreakinnuli@gmail.com>
2024-02-01 00:06:09 +0100

    Remove unnecessary files and code

--- back-end/routes/localFileTranscribe.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router()
-const transcribeController = require('../controller/transcribeController')
-
-router.route('/')
-    .post(transcribeController.sendFile)
-
-
-module.exports = router;
